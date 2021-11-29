8b32d1d9ccd0120ab9681a88410b99de49156729
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2021-11-29 15:00:11 -0300

    Created Edit data business

---
+++ app/src/main/java/com/example/appprestador/Business/EditDataBusiness.java
@@ -0,0 +1,53 @@
+package com.example.appprestador.Business;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.appprestador.R;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class EditDataBusiness extends AppCompatActivity {
+
+    public ImageView imgBack;
+    public TextInputEditText edtName;
+    public TextInputEditText edtCNPJ;
+    public AppCompatButton btnEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_data_business);
+
+        getSupportActionBar().hide();
+        getIds();
+        maskFormat();
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itI = new Intent(EditDataBusiness.this, MyDataBusiness.class);
+                startActivity(itI);
+            }
+        });
+    }
+
+    private void getIds() {
+        imgBack = findViewById(R.id.img_back);
+        edtName = findViewById(R.id.edt_name_business);
+        edtCNPJ = findViewById(R.id.edt_cnpj_businesss);
+        btnEdit = findViewById(R.id.btn_edit_data);
+    }
+
+    public void maskFormat() {
+        SimpleMaskFormatter mask_cnpj = new SimpleMaskFormatter("NN.NNN.NNN/NNNN-NN");
+        MaskTextWatcher mtw_cnpj = new MaskTextWatcher(edtCNPJ, mask_cnpj);
+        edtCNPJ.addTextChangedListener(mtw_cnpj);
+    }
+}
